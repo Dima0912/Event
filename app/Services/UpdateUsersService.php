a70f6672f3bc60d6d843e40ae6b59abe01c284bd
Dima <mirnydimas@gmail.com>
2022-08-11 21:54:08 +0000

    not found users

--- app/Services/UpdateUsersService.php
+++ app/Services/UpdateUsersService.php
@@ -11,11 +11,16 @@ class UpdateUsersService implements UpdateUsersInterface
 
     public function update_users(UpdateUserRequest $request, $id)
     {
+
         $user = User::find($id);
+        if (empty($user)){
+            abort(404);
+        }
         $user->name = $request->input('name');
         $user->surname = $request->input('surname');
         $user->phone = $request->input('phone');
         $user->email = $request->input('email');
         $user->save();
+
     }
 }
