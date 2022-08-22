44b842998c10a8491365b53e18fb6a204784dcad
Dima <mirnydimas@gmail.com>
2022-08-22 20:22:10 +0000

    tests

--- tests/Feature/EventControllerTest.php
+++ tests/Feature/EventControllerTest.php
@@ -2,22 +2,14 @@
 
 namespace Tests\Feature;
 
-use App\Http\Request\CreateUserRequest;
-use App\Models\User;
-use http\Env\Request;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-
-class UserTest extends TestCase
+class EventControllerTest extends TestCase
 {
-
-
     use DatabaseMigrations;
-
 
     /**
      * A basic feature test example.
@@ -34,14 +26,10 @@ class UserTest extends TestCase
         $response = $this->withHeaders([
             'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vZXZlbnRzLnRlc3QvYXBpL2xvZ2luIiwiaWF0IjoxNjYxMDI5MjI5LCJleHAiOjE2NjEwMzI4MjksIm5iZiI6MTY2MTAyOTIyOSwianRpIjoiWjJFUWx6M3EyOFJFSDJTOCIsInN1YiI6IjIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.D8RgEHdSKmw5K1Igt3R769kGoTUoWIPsxK0WE6gVPnY',
         ])
-
-
-            ->post('api/users', [
-                'name' => 'йцуукук',
-                'surname' => 'Mirny',
-                'phone' => '+380978943827',
-                'email' => 'mirnывавas@gmail.com',
-                'password' => 'secret',
+            ->post('api/events', [
+                "title" => "Year",
+                "date_start" => "2022-12-23 00:00:00",
+                "date_end" => "2023-01-07 00:00:00"
             ]);
 
         $response->assertStatus(200);
---
+++ tests/Feature/JwtControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class JwtControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAuthorizeUser()
+    {
+        $response = $this->post('api/login', [
+            'email' => 'mirnydmisds@gmail.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertOk();
+    }
+
+    public function testFailedEmailUser()
+    {
+        $response = $this->post('api/login', [
+            'email' => 'mirnydmids@gmail.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertUnauthorized();
+    }
+
+    public function testFailedPasswordUser()
+    {
+        $response = $this->post('api/login', [
+            'email' => 'mirnydmisds@gmail.com',
+            'password' => 'pasword',
+        ]);
+
+        $response->assertUnauthorized();
+    }
+}
